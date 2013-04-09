f7220515e3694cbd3ebea8bac21041edc3fdbd06
Saqib Razzaq <saqibrazzaq@gmail.com>
2013-04-09 08:23:22 +0500

    Initial Update

---
+++ Startapp.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if "%SUN_JRE_1_7_HOME%"=="" goto :app_manual
+goto :app_jre7
+
+:app_manual
+echo Running default Java
+java -cp "lib\*;.;Aspose.ExamplesCenter.jar;%EXAMPLES_DIR%\lib\*" aspose.examplescenter.desktop.Dashboard
+goto :app_end
+
+:app_jre7
+echo Running JRE 7
+echo SUN_JRE_1_7_HOME=%SUN_JRE_1_7_HOME%
+"%SUN_JRE_1_7_HOME%\bin\java" -cp "lib\*;.;Aspose.ExamplesCenter.jar;%SUN_JRE_1_7_HOME%\lib\*;%EXAMPLES_DIR%\lib\*" aspose.examplescenter.desktop.Dashboard
+goto :app_end
+
+:app_end
+echo Finished
---
+++ startapp.sh
@@ -0,0 +1,2 @@
+# Run the Java Dashboard Application
+"${SUN_JRE_1_7_HOME}/bin/java" -cp "lib/*:.:Aspose.ExamplesCenter.jar:${SUN_JRE_1_7_HOME}/lib/*:${EXAMPLES_DIR}/lib/*" aspose.examplescenter.desktop.Dashboard
