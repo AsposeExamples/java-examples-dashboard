a99dae4dbf7ccc462a183962650410073be1205c
saqibrazzaq <saqibrazzaq@gmail.com>
2013-12-05 16:06:02 +0500

    max heap

--- Startapp.bat
+++ Startapp.bat
@@ -5,13 +5,13 @@ goto :app_jre7
 
 :app_manual
 echo Running default Java
-java --Xmx1g cp "lib\*;.;Aspose.ExamplesCenter.jar;%EXAMPLES_DIR%\lib\*" aspose.examplescenter.desktop.Dashboard
+java -Xmx1g cp "lib\*;.;Aspose.ExamplesCenter.jar;%EXAMPLES_DIR%\lib\*" aspose.examplescenter.desktop.Dashboard
 goto :app_end
 
 :app_jre7
 echo Running JRE 7
 echo SUN_JRE_1_7_HOME=%SUN_JRE_1_7_HOME%
-"%SUN_JRE_1_7_HOME%\bin\java" --Xmx1g -cp "lib\*;.;Aspose.ExamplesCenter.jar;%SUN_JRE_1_7_HOME%\lib\*;%EXAMPLES_DIR%\lib\*" aspose.examplescenter.desktop.Dashboard
+"%SUN_JRE_1_7_HOME%\bin\java" -Xmx1g -cp "lib\*;.;Aspose.ExamplesCenter.jar;%SUN_JRE_1_7_HOME%\lib\*;%EXAMPLES_DIR%\lib\*" aspose.examplescenter.desktop.Dashboard
 goto :app_end
 
 :app_end
